93380a73c127a246e9c6c4c5121ecf4bc36dabb2
jessicacosta07 <jessikacostajf@gmail.com>
2023-10-17 00:24:38 -0300

    página de criação/edição criada

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using mvc_project_csharp.Context;
+using mvc_project_csharp.Models;
 
 namespace mvc_project_csharp.Controllers
 {
@@ -19,6 +20,45 @@ namespace mvc_project_csharp.Controllers
         {
             var contatos = _context.Contatos.ToList();
             return View(contatos);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
